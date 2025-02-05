1cba5c653bca6fa6ff19e543019e0659af0ba44f
Erin Zhang <nunujiarui@gmail.com>
2025-02-05 15:52:30 -0800

    Update distRcpp

---
+++ src/distRcpp.cpp
@@ -0,0 +1,33 @@
+#include "RcppArmadillo.h"
+#include "Rcpp.h"
+
+using namespace Rcpp; 
+using namespace arma;
+
+// via the depends attribute we tell Rcpp to create hooks for
+// RcppArmadillo so that the build process will know what to do
+//
+// [[Rcpp::depends(RcppArmadillo)]]
+// [[Rcpp::depends(Rcpp)]]
+
+Rcpp::NumericMatrix distRcpp(Rcpp::NumericMatrix X, String metric){
+  Rcpp::NumericMatrix XX(X);
+  //Rcpp::Rcout << X << endl; 
+  //Rcpp::Rcout << XX << endl;    
+  int p = XX.nrow();
+  Rcpp::NumericMatrix DIST(p,p);
+  for(int i=0; i< p; i++){
+    for(int j=0; j< p; j++){
+      if(i!=j){
+        if(metric == "euclidean"){
+          // Euclidean distance
+          DIST(i,j)=sqrt(sum((XX.row(i)-XX.row(j))*(XX.row(i)-XX.row(j))));
+        }else if (metric == "cosine"){
+          // Cosine distance
+          DIST(i,j)= 1-sum(XX.row(i)*XX.row(j))/(sqrt(sum(pow(XX.row(i),2)))*sqrt(sum(pow(XX.row(j),2))));
+        }
+      }
+    }    
+  }
+  return DIST;
+}   
