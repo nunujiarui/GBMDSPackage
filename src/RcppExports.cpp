46e204b1176f67c371e2e6847d95acb8f50c35b2
Erin Zhang <nunujiarui@gmail.com>
2025-02-04 16:45:08 -0800

    update initialFun_cpp.h

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -6,10 +6,12 @@
 #include "bisectionFun_cpp.h"
 #include "dproposalFun_cpp.h"
 #include "rmvnorm_arma.h"
+#include "initialFun_cpp.h"
 #include "initialFun_SN_cpp.h"
 #include "initialFun_SN_incr_cpp.h"
 #include "initialFun_T_cpp.h"
 #include "initialFun_T_incr_cpp.h"
+
 
 #include "likelihoodFun_cpp.h"
 #include "likelihoodFun_SN_cpp.h"
