8a06e291e51c54372dfed502c84c0b751e2a341a
Erin Zhang <nunujiarui@gmail.com>
2025-02-05 17:21:44 -0800

    Update inplace_tri_mat_mult

--- src/inplace_tri_mat_mult.h
+++ src/inplace_tri_mat_mult.h
@@ -1,17 +1,15 @@
+#ifndef INPLACE_TRI_MAT_MULT_H
+#define INPLACE_TRI_MAT_MULT_H
+
 #include <RcppArmadillo.h>
+
+using namespace Rcpp;
 using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
 
-// static double const log2pi = std::log(2.0 * M_PI);
+// [[Rcpp::export]]
 
 /* C++ version of the dtrmv BLAS function */
-void inplace_tri_mat_mult(arma::rowvec &x, arma::mat const &trimat){
-  arma::uword const n = trimat.n_cols;
-  
-  for(unsigned j = n; j-- > 0;){
-    double tmp(0.);
-    for(unsigned i = 0; i <= j; ++i)
-      tmp += trimat.at(i, j) * x[i];
-    x[j] = tmp;
-  }
-}
+void inplace_tri_mat_mult(arma::rowvec &x, arma::mat const &trimat);
+
+#endif
