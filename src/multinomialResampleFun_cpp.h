f5c43ce8cd238c7638e19c36fac2e5199821c4cf
Erin Zhang <nunujiarui@gmail.com>
2025-02-05 22:26:15 -0800

    Update multinomialResampleFun_cpp.h

--- src/multinomialResampleFun_cpp.h
+++ src/multinomialResampleFun_cpp.h
@@ -1,23 +1,12 @@
-#include <Rcpp.h>
+#ifndef DISTRCPP_H
+#define DISTRCPP_H
+
 using namespace Rcpp;
+
 
 // [[Rcpp::export]]
 
 // Obtain resampling index using multinomial resampling
-Rcpp::NumericVector multinomialResampleFun_cpp(const std::vector<double>& W) {
-  int N = W.size();
-  //std::vector<int> index(N);
-  
-  // Obtaining namespace of Matrix package
-  // Environment pkg = Environment::namespace_env("base");
-  
-  // Picking up Matrix() function from Matrix package
-  // Function f = pkg["sample.int"];
-  
-  // calling rnorm()
-  Function f("sample.int");   
-  
-  return f(Named("n", N), Named("size", N), Named("replace", true), Named("prob", W));
-  // return index;
-}
+Rcpp::NumericVector multinomialResampleFun_cpp(const std::vector<double>& W);
 
+#endif
