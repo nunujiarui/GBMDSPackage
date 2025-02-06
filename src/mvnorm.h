7c000255487214f298ec6d67384eedeacb7f9a96
Erin Zhang <nunujiarui@gmail.com>
2025-02-05 22:46:06 -0800

    Update mvnorm.h

--- src/mvnorm.h
+++ src/mvnorm.h
@@ -1,14 +1,14 @@
-#ifndef CHOLESKY_TRIDIAGONAL_H
-#define CHOLESKY_TRIDIAGONAL_H
-
-#ifndef FORWARD_ALGORITHM_H
-#define FORWARD_ALGORITHM_H
-
-#ifndef BACKWARD_ALGORITHM_H
-#define BACKWARD_ALGORITHM_H
-
-#ifndef RMVNORM_ARMA_STOCHVOL_H
-#define RMVNORM_ARMA_STOCHVOL_H
+// #ifndef CHOLESKY_TRIDIAGONAL_H
+// #define CHOLESKY_TRIDIAGONAL_H
+// 
+// #ifndef FORWARD_ALGORITHM_H
+// #define FORWARD_ALGORITHM_H
+// 
+// #ifndef BACKWARD_ALGORITHM_H
+// #define BACKWARD_ALGORITHM_H
+// 
+// #ifndef RMVNORM_ARMA_STOCHVOL_H
+// #define RMVNORM_ARMA_STOCHVOL_H
 
 #include <RcppArmadillo.h>
 
@@ -29,5 +29,5 @@ vec backward_algorithm(const vec& chol_diag, const vec& chol_offdiag,
 
 mat rmvnorm_arma_stochvol(int n, mat precision, vec location);
 
-#endif
+// #endif
 
