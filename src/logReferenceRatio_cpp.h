540b8bb3a6b3a3d013ddfcf1fdf73cf56a7a59e5
Erin Zhang <nunujiarui@gmail.com>
2025-02-05 17:49:04 -0800

    Update logreferenceratio_ratio

--- src/logReferenceRatio_cpp.h
+++ src/logReferenceRatio_cpp.h
@@ -1,20 +1,17 @@
+#ifndef LOGREFERENCERATIO_CPP_H
+#define LOGREFERENCERATIO_CPP_H
+
 #include <RcppArmadillo.h>
+
+using namespace Rcpp;
 using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
-#include "reference_d_x_cpp.h"
-using namespace Rcpp;
+
 // [[Rcpp::export]]
 
 // Calculate the reference ratio in log scale
 
 double logReferenceRatio_cpp(arma::mat new_x, arma::mat current_x,
-                             arma::mat prev_x){
-  
-  double sum1 = sum(reference_d_x_cpp(new_x, prev_x));
-  double sum2 = sum(reference_d_x_cpp(current_x, prev_x));
-  
-  double ratio = sum1 - sum2;
-  
-  return (ratio);
-  
-}
+                             arma::mat prev_x);
+
+#endif
