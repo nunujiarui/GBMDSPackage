d70c575141c7f02badefb40b6ac25806ff6b7bd8
Erin Zhang <nunujiarui@gmail.com>
2025-02-06 11:56:33 -0800

    Update inplace_tri_mat_mult

--- src/dmvnrm_arma_fast.h
+++ src/dmvnrm_arma_fast.h
@@ -1,12 +1,15 @@
-#ifndef DMVNRN_ARMA_FAST_H
-#define DMVNRN_ARMA_FAST_H
+// #ifndef DMVNRN_ARMA_FAST_H
+// #define DMVNRN_ARMA_FAST_H
 
 #include <RcppArmadillo.h>
-#include "inplace_tri_mat_mult.h"
+// #include "inplace_tri_mat_mult.h"
 
 using namespace Rcpp;
 using namespace arma;
 // [[Rcpp::depends(RcppArmadillo)]]
+
+void inplace_tri_mat_mult(arma::rowvec &x, arma::mat const &trimat);
+
 
 // [[Rcpp::export]]
 arma::vec dmvnrm_arma_fast(arma::mat const &x,  
@@ -14,4 +17,4 @@ arma::vec dmvnrm_arma_fast(arma::mat const &x,
                            arma::mat const &sigma, 
                            bool const logd = false);
 
-#endif
+// #endif
